4f67ec6f1bea3c586b4b0d9115d370a5dccce905
bianchui <bianchui@gmail.com>
2022-07-26 20:08:37 +0800

    [BC]STKCachedDataSource

---
+++ StreamingKit/StreamingKit/STKCachedDataSource.h
@@ -0,0 +1,9 @@
+//
+//  STKCachedDataSource.m
+//  StreamingKit
+//
+//  Created by bianchui on 2022/7/26.
+//  Copyright © 2022 bianchui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
